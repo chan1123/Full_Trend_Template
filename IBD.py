d88f3fc71097a17db012d9faf64dd26ffb725033
Edmund Chan <chan1123581321@yahoo.com.hk>
2021-04-04 16:34:18 +0800

    Optimise IBD.py

--- IBD.py
+++ IBD.py
@@ -1,6 +1,5 @@
-import pandas as pd
-from IBD_functions import read_ibd, convert_datatypes_ibd
 from IBD_functions import latest_file
+from IBD_functions import read_ibd, convert_datatypes_ibd
 
 df_ibd = convert_datatypes_ibd(read_ibd(latest_file()))
 
--- IBD_functions.py
+++ IBD_functions.py
@@ -1,8 +1,53 @@
 import pandas as pd
 import glob
 import re
-from IBD_variables import abcde_map, abcde_plus_map
-from IBD_variables import abcde_plus_list, abcde_list, numeric_list
+
+abcde_map = {
+    'A': 5,
+    'B': 4,
+    'C': 3,
+    'D': 2,
+    'E': 1
+}
+
+abcde_plus_map = {
+    'A+': 13, 'A': 12, 'A-': 11,
+    'B+': 10, 'B': 9, 'B-': 8,
+    'C+': 7, 'C': 6, 'C-': 5,
+    'D+': 4, 'D': 3, 'D-': 2,
+    'E': 1
+}
+
+numeric_list = [
+    'Price',
+    'Price $ Change',
+    'Price % Change',
+    'Comp. Rating',
+    'EPS Rating',
+    'RS Rating',
+    'Vol. % Change',
+    'Vol. (1000s)',
+    # 'Industry Group Rank',
+    # '# of Funds - last reported qrtr',
+    # 'Pretax Margin',
+    # 'Curr Yr EPS Est. % Chg.',
+    # 'Curr Qtr EPS Est. % Chg.',
+    # 'Last Qtr EPS % Chg.',
+    # 'Last Qtr Sales % Chg.',
+    # 'Div Yield',
+    # 'PE Ratio',
+    # 'Vol- 50 Day Avg. (1000s)'
+]
+
+abcde_list = [
+    'SMR Rating',
+    'Spon Rating'
+]
+
+abcde_plus_list = [
+    'Ind Grp RS',
+    'Acc/Dis Rating'
+]
 
 
 def read_ibd(filename):
@@ -53,7 +98,6 @@ def latest_file():
     file_list = glob.glob("assets/IBD_Excel/*.xlsx")
 
     combined_str = ''.join(file_list)
-
 
     file_date = re.findall(r'Excel\/(.*?)\_', combined_str)
     filename = 'assets/IBD_Excel/' + max(file_date) + '_IBD.xlsx'
